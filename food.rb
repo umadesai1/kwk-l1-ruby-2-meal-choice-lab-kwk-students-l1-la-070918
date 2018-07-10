675a11330e17e38090610407575d8951dc0ddcf7
Uma Desai <umadesai38@gmail.com>
2018-07-10 18:14:37 +0000

    automatically backed up by learn

--- food.rb
+++ food.rb
@@ -1,8 +1,8 @@
-def breakfest(food="Frosted Flakes")
+def breakfast(food="Frosted Flakes")
   puts "Morning is the best time for #{food}!"
 end
 
-breakfest
+breakfast
 
 def lunch(food="grilled cheese")
   puts "Lunch is the best time for #{food}!"
