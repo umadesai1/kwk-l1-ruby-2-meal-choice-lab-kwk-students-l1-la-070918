d3256e564e52e9e1825d7fae8b3e595c71aa4653
Uma Desai <umadesai38@gmail.com>
2018-07-10 18:24:47 +0000

    automatically backed up by learn

--- food.rb
+++ food.rb
@@ -4,19 +4,19 @@ end
 
 
 def breakfast(food="frosted flakes")
-  puts "Morning is the best time for #{food}!"
+  "Morning is the best time for #{food}!"
 end
 
 breakfast
 
 def lunch(food="grilled cheese")
-  puts "Lunch is the best time for #{food}!"
+  "Lunch is the best time for #{food}!"
 end
 
 lunch
 
 def dinner(food="salmon")
-  puts "Dinner is the best time for #{food}!"
+  "Dinner is the best time for #{food}!"
 end
 
 dinner
